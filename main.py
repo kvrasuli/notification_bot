ae964e1a91b82b059062cfb521e5b1e50b3e7f45
Kirill Rasuli <rasulikv@gmail.com>
2020-07-31 11:39:17 +0300

    2nd. exc handling reworked, timestamp handling for 'found' added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import telegram
 from dotenv import load_dotenv
 import os
+import time
 
 def run_bot(devman_token, telegram_token, chat_id):
     url = 'https://dvmn.org/api/long_polling/'
@@ -10,29 +11,37 @@ def run_bot(devman_token, telegram_token, chat_id):
     bot = telegram.Bot(token=telegram_token)
     while True:
         try:
-            response = requests.get(url, headers=headers, params=params, timeout=120)
-            if response.json()['status'] == 'timeout':
-                params['timestamp'] = response.json()['timestamp_to_request']
-            elif response.json()['status'] == 'found':
-                for attempt in response.json()['new_attempts']:
+            response = requests.get(url)
+            response = requests.get(url, headers=headers, params=params, timeout=90)
+            response.raise_for_status()
+            response_decoded = response.json()
+            if response_decoded['status'] == 'timeout':
+                params['timestamp'] = response_decoded['timestamp_to_request']
+            elif response_decoded['status'] == 'found':
+                params['timestamp'] = response_decoded['last_attempt_timestamp']
+                for attempt in response_decoded['new_attempts']:
                     message = f"The lesson \"{attempt['lesson_title']}\" has been assessed!"
                     if attempt['is_negative']:
-                        message += 'Give it a one more try.'
+                        message += ' Give it a one more try.'
                         bot.send_message(chat_id=chat_id, text=message)
                     else:
-                        message += 'Everything is OK, go ahead!'
+                        message += ' Everything is OK, go ahead!'
                         bot.send_message(chat_id=chat_id, text=message)
         except requests.exceptions.ReadTimeout:
-            print('Timeout!')
+            pass
         except ConnectionError:
-            print('Connection error.')
+            time.sleep(60)
 
-if __name__ == '__main__':
+def main():
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     devman_token = os.getenv('DEVMAN_TOKEN')
     chat_id = os.getenv('CHAT_ID')
     run_bot(devman_token, telegram_token, chat_id)
+
+if __name__ == '__main__':
+    main()
+
 
 
     
